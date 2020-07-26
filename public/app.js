87d3faf6115cbff05be97c72550866ad94b66abe
andrew <elbroandrew@gmail.com>
2020-07-26 19:22:44 +1000

    added deleteTodo graphQL

--- public/app.js
+++ public/app.js
@@ -62,14 +62,23 @@
         //this.todoTitle = ''
       },
       removeTodo(id) {
-        fetch("/api/todo/" + id, {
-          method: "delete"
+        const query = `
+          mutation {
+            deleteTodo(id: "${id}")
+          }
+        `
+        fetch("/graphql", {
+          method: "post",
+          headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+          },
+          body: JSON.stringify({query})
         })
           .then(()=>{
             this.todos = this.todos.filter(t => t.id !== id)
           })
           .catch(e => console.log(e))
-        this.todos = this.todos.filter(t => t.id !== id)
       },
       completeTodo(id){
         const query = `
